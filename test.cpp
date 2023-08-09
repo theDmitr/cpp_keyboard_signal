0baf208dd483a1c574911d161eddc9e39974f745
Dmitry <113686410+theDmitr@users.noreply.github.com>
2023-08-09 15:41:56 +0300

    clear

--- test.cpp
+++ test.cpp
@@ -4,26 +4,19 @@
 #include <fstream>
 #include <Windows.h>
 #include <winuser.h>
-//#include <winable.h> /* Dev-C++ specific */
 using namespace std;
  
-/* HWND = "Window Handle" */
-
- 
-/* This is a function to simplify usage of sending keys */
 void GenerateKey(int vk, BOOL bExtended) {
  
     KEYBDINPUT  kb = {0};
     INPUT       Input = {0};
- 
-    /* Generate a "key down" */
+	
     if (bExtended) { kb.dwFlags  = KEYEVENTF_EXTENDEDKEY; }
     kb.wVk  = vk;
     Input.type  = INPUT_KEYBOARD;
     Input.ki  = kb;
     SendInput(1, &Input, sizeof(Input));
- 
-    /* Generate a "key up" */
+	
     ZeroMemory(&kb, sizeof(KEYBDINPUT));
     ZeroMemory(&Input, sizeof(INPUT));
     kb.dwFlags  =  KEYEVENTF_KEYUP;
@@ -42,7 +35,7 @@ int main()
 	
 	while (true)
 	{
-		HWND GameWindow = FindWindow(0, "Malinovka");
+		HWND GameWindow = FindWindow(0, "myWindow");
 		SetForegroundWindow(GameWindow);
 		GenerateKey('A', FALSE);
 		Sleep(5000);
